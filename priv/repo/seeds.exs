0f044b9c56c5661653c72e371734951989339ae6
Chase Philips <chasephilips@chases-mbp.home>
2020-08-15 22:48:10 -0400

    make seeds better

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+seconds_in_day = 86_400
+
+tomorrow = DateTime.add(DateTime.utc_now(), seconds_in_day, :seconds)
+dayafter = DateTime.add(DateTime.utc_now(), seconds_in_day * 2, :seconds)
+dayafterafter = DateTime.add(DateTime.utc_now(), seconds_in_day * 3, :seconds)
+
+Lknvball.Events.create_event(%{
+  name: "Blind Draw Thursday",
+  cost: 20,
+  start_time: DateTime.utc_now()
+})
+
+Lknvball.Events.create_event(%{name: "Blind Draw Friday", cost: 20, start_time: tomorrow})
+Lknvball.Events.create_event(%{name: "Mix n Match Saturday", cost: 20, start_time: dayafter})
+Lknvball.Events.create_event(%{name: "Blind Draw Saturday", cost: 20, start_time: dayafterafter})
+
+Lknvball.Accounts.create_user(%{name: "User 1", email: "user1@gmail.com", image: "none"})
+Lknvball.Accounts.create_user(%{name: "User 2", email: "user2@gmail.com", image: "none"})
