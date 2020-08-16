94cb27bb10b95a1a850ed21af8373d5c6ceac209
Chase Philips <chasephilips@chases-mbp.home>
2020-08-15 22:47:52 -0400

    fix bad schema

--- lib/lknvball_web/schema/schema.ex
+++ lib/lknvball_web/schema/schema.ex
@@ -5,22 +5,27 @@ defmodule LknvballWeb.Schema do
   use Absinthe.Relay.Schema.Notation, :modern
 
   import_types(LknvballWeb.Schema.User)
+  import_types(LknvballWeb.Schema.Event)
 
   query do
     import_fields(:user_queries)
+    import_fields(:event_queries)
   end
 
   mutation do
     import_fields(:user_mutations)
   end
 
-  subscription do
-  end
+  # subscription do
+  # end
 
   node interface do
     resolve_type(fn
       %Lknvball.Accounts.User{}, _ ->
         :user
+
+      %Lknvball.Events.Event{}, _ ->
+        :event
 
       _, _ ->
         nil
