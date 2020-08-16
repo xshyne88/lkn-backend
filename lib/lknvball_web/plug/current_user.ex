e3f723bcafc133d4454e32d68846a5032ab8e048
Chase Philips <chasephilips@chases-mbp.home>
2020-08-15 22:47:59 -0400

    add current user setter on context

---
+++ lib/lknvball_web/plug/current_user.ex
@@ -0,0 +1,21 @@
+defmodule VisdocsWeb.Plug.AbsintheContext do
+  @moduledoc false
+
+  @behaviour Plug
+
+  import Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(%{remote_ip: remote_ip} = conn, _) do
+    IO.inspect(conn)
+
+    case Guardian.Plug.current_resource(conn) do
+      nil ->
+        put_private(conn, :absinthe, %{context: %{remote_ip: remote_ip}})
+
+      user ->
+        put_private(conn, :absinthe, %{context: %{current_user: user, remote_ip: remote_ip}})
+    end
+  end
+end
