72514fafb2ceee0b0bd6db6b111db5d95d1a6c67
Chase Philips <chasephilips@chases-mbp.home>
2020-08-18 22:56:15 -0400

    no idea what this does lol

--- lib/lknvball_web/auth/guardian.ex
+++ lib/lknvball_web/auth/guardian.ex
@@ -1,18 +1,19 @@
 defmodule LknvballWeb.Guardian do
   use Guardian, otp_app: :lknvball
 
-  def subject_for_token(res, _claims) do
-    # need to add authenticity check and expiration
-    {:ok, res}
+  def subject_for_token(res, claims) do
+    sub = res.id
+    {:ok, sub}
   end
 
   # def subject_for_token(_, _) do
   #   {:error, :notfound}
   # end
 
-  def resource_from_claims(res) do
+  def resource_from_claims(claims) do
+    id = claims["sub"]
     # need to add authenticity check and expiration
-    {:ok, res}
+    {:ok, nil}
   end
 
   # def resource_from_claims(_) do
