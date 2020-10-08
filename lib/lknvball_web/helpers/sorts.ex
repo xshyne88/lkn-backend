5c2a9e7780972c03afb82a0c2a807a025e3d77e5
Benjamin Leeds <bleeds@samaritan.org>
2020-10-07 22:42:32 -0400

    document: Sort functions

--- lib/lknvball_web/helpers/sorts.ex
+++ lib/lknvball_web/helpers/sorts.ex
@@ -1,6 +1,7 @@
 defmodule LknvballWeb.Helpers.Sorts do
   import Ecto.Query
 
+  # TODO: Make sorts order safe
   def apply_to_query(query, %{sort_by: sort_by}) do
     order_by_list = create_order_by_list(sort_by)
 
@@ -14,9 +15,8 @@ defmodule LknvballWeb.Helpers.Sorts do
 
   defp create_order_by_list(sort_by_field_map) do # todo: add a guard here?
     sort_by_field_map
-    |> Enum.to_list
+    |> Enum.to_list # List will be sorted alphabetically by the key
     |> Enum.map(&flip_tuple/1)
-    # |> Enum.map(&convert_value_to_direction/1)
   end
 
   defp flip_tuple({a, b}) do
